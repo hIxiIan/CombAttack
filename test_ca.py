0148204c04a3ccedb5de2b5f3beddc9495ec8005
hixian <316840383@qq.com>
2021-10-17 10:22:04 +0800

    10 repeat experiments

--- test_ca.py
+++ test_ca.py
@@ -292,7 +292,7 @@ if __name__ == '__main__':
 
     rootdir = "result" + os.sep
     prefix = cmd.dataset + "_no_wrong_label_"
-    times = 10
+    times = 5
     seeds = [2012, 1997, 5018, 2413, 97, 21, 32, 56, 44, 94]
     for i in range(times):
         cmd.seed = seeds[i]
