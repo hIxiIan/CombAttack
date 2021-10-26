62ede99f815fb4722af8ab5213713ca801079f25
抱月 <316840383@qq.com>
2021-10-26 18:53:47 +0800

    test

--- ca.py
+++ ca.py
@@ -236,7 +236,7 @@ def run(subgraph_type, cmd=None, with_w_label=False, sample_ratio=0.05, p=2.0, q
     random.seed(cmd.seed)
     targets = random.sample(list(splits.test_nodes), 1000)
     args = ARGS(cmd=cmd, targets=targets, splits=splits)
-
+    print(args.device)
 
     args.subgraph_type = subgraph_type
     args.with_w_label = with_w_label
