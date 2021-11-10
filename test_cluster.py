912d2c7199ecdf3ee354ddefadbebd0a2e54d64c
hixian <316840383@qq.com>
2021-11-10 22:43:32 +0800

    test_cluster

--- test_cluster.py
+++ test_cluster.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     parser.add_argument("-in_da", "--indirect_attack", action="store_true", help="indirect attack")
     parser.add_argument("-tn", "--target_nums", default=100, type=int, help="target nums")
 
-    parser.add_argument("--dataset", default="cora", type=str, help="dataset")
+    parser.add_argument("--dataset", default="", type=str, help="dataset")
     parser.add_argument("--n_us", action="store_true", help="run sga model")
     parser.add_argument("-p", default=7.0, type=float)
     parser.add_argument("-q", default=0.25, type=float)
