5c3e0c4c7e51303bd45e3225e4e00d6a0670bd1a
抱月 <316840383@qq.com>
2021-11-17 15:22:06 +0800

    间接攻击，获取边numba实现

--- cluster.py
+++ cluster.py
@@ -83,7 +83,7 @@ class Cluster:
         for i in range(self.n_classes):
             farthest_idx[i] = np.array(df.iloc[i].sort_values(ascending=False).index)
         self.farthest_idx = farthest_idx
-        # print('cluster results:{}'.format(self.farthest_idx))
+        print('\ncluster results:{}'.format(self.farthest_idx))
 
     def visualization(self):
         # print('targets labels:{}'.format(list(self.cluster_label_pred)))
