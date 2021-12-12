2d420649221c3bd70433e2e56586c1a3baa4e547
hixian <316840383@qq.com>
2021-12-12 13:04:55 +0800

    用预测labels统计簇的类别是否是误分类类别

--- ca.py
+++ ca.py
@@ -300,11 +300,11 @@ def testACC(attacked_models, attacker, args, verbose=True, verbose_us=False):
     for attacked_model in attacked_models:
         name = attacked_model.name
         if attacked_model.is_dr:
-            sur_pred = gf.get('softmax')(attacked_model.predict().detach().cpu().numpy())
-            original_predicts[name] = sur_pred[args.targets]
+            sur_preds = gf.get('softmax')(attacked_model.predict().detach().cpu().numpy())
+            original_predicts[name] = sur_preds[args.targets]
         else:
-            sur_pred = attacked_model.predict(np.arange(len(args.node_label)), transform="softmax")
-            original_predicts[name] = sur_pred[args.targets]
+            sur_preds = attacked_model.predict(np.arange(len(args.node_label)), transform="softmax")
+            original_predicts[name] = sur_preds[args.targets]
 
         eva_res[name] = np.zeros(len(args.targets)).astype('bool')
         # eva_res_wl[name] = np.zeros(len(args.targets)).astype('bool')
@@ -312,7 +312,7 @@ def testACC(attacked_models, attacker, args, verbose=True, verbose_us=False):
         # poi_res_wl[name] = np.zeros(len(args.targets)).astype('bool')
 
         if sampler is not None:
-            sur_labels = np.array([lo.argmax() for lo in sur_pred])
+            sur_labels = np.array([lo.argmax() for lo in sur_preds])
             wrong_labels = get_wrong_labels(attacker.logits, args.targets, sur_labels)
             add_gcn_labels, add_gcn_labels_rate = mapCluster2GCN(args.targets, sur_labels, sampler.added_edges)
             add_clusterIsMisClassifiedLabel[name] = wrong_labels == add_gcn_labels
