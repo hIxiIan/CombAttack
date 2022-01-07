18aca46c6417d535b42e5591da45b2ff99fc3fdf
抱月 <316840383@qq.com>
2022-01-07 17:25:10 +0800

    fix numba；添加丹总模式和numba模式切换

--- tedge.py
+++ tedge.py
@@ -209,7 +209,7 @@ class tGraph(object):
 class tGraphNE(object):
     def __init__(self, tG, time_biased_type, first_biased_type, amount_biased, alpha, output,
                  dimensions=128, num_walks=4, walk_length=10, output_pklG=False,
-                 window_size=4, workers=1, hs=1, seed=2022, verbose=0, is_test_tedge_edges=False):
+                 window_size=4, workers=1, hs=1, seed=2022, verbose=0, is_test_tedge_edges=False, is_dan=True):
         self.G = tG.G
         self.min_time = tG.min_time
         self.max_time = tG.max_time
@@ -220,7 +220,10 @@ class tGraphNE(object):
         self.amount_biased = amount_biased
         self.alpha = alpha
         t1 = time.time()
-        walks = self.simulate_walks(num_walks, walk_length)  # 随机游走
+        if is_dan:
+            walks = self.dan_simulate_walks(num_walks, walk_length)  # 随机游走
+        else:
+            walks = self.simulate_walks(num_walks, walk_length)  # 随机游走
         t2 = time.time()
         word2vec_model = Word2Vec(sentences=walks, vector_size=dimensions, window=window_size, min_count=0, sg=1, hs=1,
                                   workers=workers, seed=seed)
@@ -276,24 +279,20 @@ class tGraphNE(object):
 
         cur = start_node
         cur_nbrs = np.array(list(G.neighbors(cur)))
-        weight_keys = []
         nbr_keys = []
         for nbr in cur_nbrs:
             nbr_key = list(G.get_edge_data(cur, nbr))  # cur领边的key数组
             nbr_keys.append(nbr_key)
-            weight_keys.append([G[cur][nbr][nk]['weight'] for nk in nbr_key])
         nbr_keys = np.array(make_redundancy(nbr_keys), dtype=np.int32)
-        weight_keys = np.array(make_redundancy(weight_keys), dtype=np.float32)
+        if len(cur_nbrs) == 0:
+            return walk
 
         unnormalized_probs_t, tmp_node, tmp_time, tmp_key = get_first_step(cur_nbrs, nbr_keys, self.first_biased_type, self.max_time, self.min_time)
-        selected = weight_choice(unnormalized_probs_t)
-        next_node = tmp_node[selected]
-        next_time = tmp_time[selected]
-        next_key = tmp_key[selected]
-        if next_node is not None:
-            walk.append(next_node)
-            walk_time.append(next_time)
-            walk_edge.append(next_key)
+        if unnormalized_probs_t is not None and len(unnormalized_probs_t) > 0:
+            selected = weight_choice(unnormalized_probs_t)
+            walk.append(tmp_node[selected])
+            walk_time.append(tmp_time[selected])
+            walk_edge.append(tmp_key[selected])
         else:
             return walk
 
@@ -309,11 +308,66 @@ class tGraphNE(object):
                 weight_keys.append([G[cur][nbr][nk]['weight'] for nk in nbr_key])
             nbr_keys = np.array(make_redundancy(nbr_keys), dtype=np.int32)
             weight_keys = np.array(make_redundancy(weight_keys), dtype=np.float32)
+            if len(cur_nbrs) == 0 or len(nbr_keys) == 0:
+                break
+
             unnormalized_probs_t, tmp_node, tmp_time, tmp_key = get_next_step(cur_nbrs, nbr_keys, weight_keys, self.time_biased_type, self.amount_biased, self.max_time, self.max_time, prevtime)
-            selected = weight_choice(unnormalized_probs_t)
-            next_node = tmp_node[selected]
-            next_time = tmp_time[selected]
-            next_key = tmp_key[selected]
+            if unnormalized_probs_t is not None and len(unnormalized_probs_t) > 0:
+                selected = weight_choice(unnormalized_probs_t)
+                walk.append(tmp_node[selected])
+                walk_time.append(tmp_time[selected])
+                walk_edge.append(tmp_key[selected])
+            else:
+                break
+        return walk
+
+    def dan_simulate_walks(self, num_walks, walk_length):
+        """
+        Repeatedly simulate random walks from each node.
+        对每个结点，根据num_walks得出其多条随机游走路径
+
+        """
+        G = self.G
+        walks = []
+        nodes = list(G.nodes())
+        if self.verbose > 0:
+            print("Walk iteration:")
+        for walk_iter in range(num_walks):
+            if self.verbose > 0:
+                print(str(walk_iter + 1), '/', str(num_walks))
+            random.shuffle(nodes)
+            for node in nodes:
+                walks.append(self.dan_temporal_walk(walk_length=walk_length, start_node=node))
+        return walks
+
+    def dan_temporal_walk(self, walk_length, start_node):
+        """
+        功能： 从一个初始结点计算一个随机游走
+        输入：
+        walk_length: 随机游走序列长度
+        start_node: 初始结点
+        返回：
+        列表，随机游走序列
+        """
+
+        walk = [start_node]  # 类型：list
+        walk_edge = []
+        walk_time = []  ##类型：list, 大小比walk的小1
+        # walk_key = []
+
+        cur = start_node
+        next_node, next_time, next_key = self.get_first_step(cur)
+        if next_node is not None:
+            walk.append(next_node)
+            walk_time.append(next_time)
+            walk_edge.append(next_key)
+        else:
+            return walk
+
+        while len(walk) < walk_length:
+            prevtime = walk_time[-1]
+            cur = walk[-1]  # 名为walk的list的最后一个元素，当前游走到的结点
+            next_node, next_time, next_key = self.get_next_step(cur, prevtime)
             if next_node is not None:
                 walk.append(next_node)
                 walk_time.append(next_time)
@@ -321,6 +375,147 @@ class tGraphNE(object):
             else:
                 break
         return walk
+
+    def get_first_step(self, cur):
+        G = self.G
+        tmp_key = []
+        tmp_node = []
+        tmp_time = []
+        unnormalized_probs_t = []
+
+        cur_nbrs = list(G.neighbors(cur))
+        if self.time_biased_type == "simple_graph":  # DeepWalk
+            for nbr in cur_nbrs:
+                tmp_node.append(nbr)
+                unnormalized_probs_t.append(1)
+
+            if len(unnormalized_probs_t) > 0:
+                idx = weight_choice(unnormalized_probs_t)
+                next_node = tmp_node[idx]
+                next_time = 0
+                next_key = 0
+                return next_node, next_time, next_key
+            else:
+                return None, None, None  # 没有符合条件的
+
+        else:
+            for nbr in cur_nbrs:
+                nbr_key = list(G.get_edge_data(cur, nbr))  # cur领边的key数组
+                for k in nbr_key:
+                    t = k
+                    if self.first_biased_type == "time_uniform":
+                        unnormalized_probs_t.append(1)
+                    elif self.first_biased_type == "time_freq":
+                        unnormalized_probs_t.append(self.max_time - t + 1)
+                    elif self.first_biased_type == "time_close_linear":
+                        unnormalized_probs_t.append(self.max_time - t + 1)
+                    elif self.first_biased_type == "time_far":
+                        unnormalized_probs_t.append(t - self.min_time + 1)
+                    elif self.first_biased_type == "time_far_linear":
+                        unnormalized_probs_t.append(t)
+
+                    tmp_node.append(nbr)
+                    tmp_time.append(t)
+                    tmp_key.append(k)
+
+            if self.first_biased_type == "time_close_linear":  # TBS descending
+                unnormalized_probs_t = linear_rank_mapping(unnormalized_probs_t, order='descending')
+            elif self.first_biased_type == "time_far_linear":  # TBS ascending
+                unnormalized_probs_t = linear_rank_mapping(unnormalized_probs_t)
+
+            if len(unnormalized_probs_t) > 0:  # 有符合条件的下一个点
+                selected = weight_choice(unnormalized_probs_t)
+                next_node = tmp_node[selected]
+                next_time = tmp_time[selected]
+                next_key = tmp_key[selected]
+                return next_node, next_time, next_key
+            else:
+                return None, None, None  # 没有符合条件的
+
+    def get_next_step(self, cur, prevtime=0):
+        """
+        功能：给定一个当前随机游走到的结点cur，这个两个相连的结点（可能有多条边），得出
+        输出：
+        #return J, q
+        直接输出下一个节点，以及时间戳
+        """
+        G = self.G
+
+        tmp_key = []
+        tmp_node = []
+        tmp_time = []
+        unnormalized_probs_t = []
+        unnormalized_probs_a = []
+
+        cur_nbrs = list(G.neighbors(cur))
+        if self.time_biased_type == "simple_graph":  # DeepWalk
+            for nbr in cur_nbrs:
+                tmp_node.append(nbr)
+                unnormalized_probs_t.append(1)
+
+            if len(unnormalized_probs_t) > 0:
+                idx = weight_choice(unnormalized_probs_t)
+                next_node = tmp_node[idx]
+                next_time = 0
+                next_key = 0
+                return next_node, next_time, next_key
+            else:
+                return None, None, None  # 没有符合条件的
+        else:
+            for nbr in cur_nbrs:
+                nbr_key = list(G.get_edge_data(cur, nbr))  # cur领边的key数组
+                for k in nbr_key:
+                    t = k
+                    a = G[cur][nbr][k]['weight']
+                    if self.time_biased_type == "no_time_limit":
+                        unnormalized_probs_t.append(1)
+
+                    elif t >= prevtime:
+                        unnormalized_probs_a.append(a)
+
+                        if self.time_biased_type == "time_uniform":
+                            unnormalized_probs_t.append(1)
+                        elif self.time_biased_type == "time_close_raw":
+                            unnormalized_probs_t.append(self.max_time - t + 1)
+                        elif self.time_biased_type == "time_close_exp":
+                            unnormalized_probs_t.append(t - prevtime)
+                        else:
+                            unnormalized_probs_t.append(t - prevtime + 1)
+                        tmp_time.append(t)
+                        tmp_node.append(nbr)
+                        tmp_key.append(k)
+
+            if self.time_biased_type == "time_close_linear":  # TBS descending
+                unnormalized_probs_t = linear_rank_mapping(unnormalized_probs_t, order='descending')
+            elif self.time_biased_type == "time_far_linear":  # TBS ascending
+                unnormalized_probs_t = linear_rank_mapping(unnormalized_probs_t)
+            elif self.time_biased_type == "time_freq_tanh":
+                unnormalized_probs_t = tanh(unnormalized_probs_t)
+            elif self.time_biased_type == "time_close_exp":
+                unnormalized_probs_t = softmax(unnormalized_probs_t)
+
+            # 金额偏好，映射函数缓解过小权重几乎没用
+            if self.amount_biased == "amount_linear":  # WBS ascending
+                unnormalized_probs_a = linear_rank_mapping(unnormalized_probs_a)
+            elif self.amount_biased == "amount_tanh":
+                unnormalized_probs_a = tanh(unnormalized_probs_a)
+            elif self.amount_biased == "amount_exp":
+                unnormalized_probs_a = softmax(unnormalized_probs_a)
+
+            if len(unnormalized_probs_t) > 0:  # 有符合条件的下一个点
+                if self.amount_biased != "amount_uniform":
+                    unnormalized_probs = combine_probs(unnormalized_probs_t, unnormalized_probs_a, self.alpha)
+                else:
+                    unnormalized_probs = unnormalized_probs_t
+
+                selected = weight_choice(unnormalized_probs)
+                next_node = tmp_node[selected]
+                next_time = tmp_time[selected]
+                next_key = tmp_key[selected]
+                return next_node, next_time, next_key
+
+            else:
+                return None, None, None  # 没有符合条件的
 
 
 @njit(cache=True)
