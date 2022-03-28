f1f49ceaf12345cd599d2457b63f4fefbe276656
抱月 <316840383@qq.com>
2022-03-28 16:10:36 +0800

    fix surrogate_model_acc_mode run manny times

--- test_cluster.py
+++ test_cluster.py
@@ -143,6 +143,8 @@ if __name__ == '__main__':
         if cmd.attacker_name not in ['sga']:
             cmd.edge_flips = "true"
             cmd.run_us = "false"
+        if cmd.surrogate_model_acc_mode == "true":
+            cmd.run_us = "false"
 
         cmd.dataset = dataset
         _asr_prefix = rootdir + os.sep + "_".join([cmd.dataset, curtime])
